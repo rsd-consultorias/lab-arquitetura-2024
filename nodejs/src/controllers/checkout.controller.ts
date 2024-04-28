1dd3fdab65482ccd2850aaf1dde001e97661af9b
Rafael Dias <rafael.dias@rsdconsultoria.com.br>
2024-04-28 17:37:08 -0300

    add paypal

--- nodejs/src/controllers/checkout.controller.ts
+++ nodejs/src/controllers/checkout.controller.ts
@@ -15,7 +15,7 @@ const CHECKOUT_URL_API = '/v1/checkout';
 export class CheckoutController {
 
     constructor(
-        private checkoutRepository: CheckoutRepository, 
+        private checkoutRepository: CheckoutRepository,
         private accountQueue: AccountQueueService,
         private subscriptionQueue: SubscriptionQueueService,
         private httpServer: IHttpServer) {
@@ -71,8 +71,60 @@ export class CheckoutController {
 
             let apiResponse = new APIResponse<CheckoutSummary>(true, undefined, checkoutSummary);
 
-            let a = new PayPal.PayPalService();
-            await a.createPaymentRequest({} as PayPal.dto.PayPalDTO);
+            let paymentService = new PayPal.PayPalService();
+            let paymentRequest: PayPal.dto.PayPalDTO = {
+                "intent": "sale",
+                "payer": {
+                    "payment_method": "paypal"
+                },
+                "transactions": [{
+                    "amount": {
+                        "currency": "BRL",
+                        "total": "93.00",
+                        "details": {
+                            "shipping": "11",
+                            "subtotal": "75",
+                            "shipping_discount": "1.00",
+                            "insurance": "1.00",
+                            "handling_fee": "1.00",
+                            "tax": "6.00"
+                        }
+                    },
+                    "description": "Teste de teste",
+                    "payment_options": {
+                        "allowed_payment_method": "IMMEDIATE_PAY"
+                    },
+                    "item_list": {
+                        "shipping_address": {
+                            "recipient_name": "PP Plus Recipient",
+                            "line1": "Gregório Rolim de Oliveira, 42",
+                            "line2": "JD Serrano II",
+                            "city": "Votorantim",
+                            "country_code": "BR",
+                            "postal_code": "18117-134",
+                            "state": "São Paulo",
+                            "phone": "0800-761-0880"
+                        },
+                        "items": [{
+                            "name": "handbag",
+                            "description": "Teste de teste",
+                            "quantity": 1,
+                            "price": "75",
+                            "tax": "6",
+                            "sku": "product34",
+                            "currency": "BRL"
+                        }]
+                    }
+                }],
+                "redirect_urls": {
+                    "return_url": "https://example.com/return",
+                    "cancel_url": "https://example.com/cancel"
+                }
+            };
+            let paymentResponse: PayPal.dto.PayPalDTO = await paymentService.createPaymentRequest(paymentRequest);
+            apiResponse.body!.paymentInfo = {
+                transactionResponseBody: paymentResponse
+            };
 
             return apiResponse;
         });
--- nodejs/src/infra/services/paypal.service.ts
+++ nodejs/src/infra/services/paypal.service.ts
@@ -17,8 +17,8 @@ export module PayPal {
             });
 
             let response = await request.json();
-
-            return response.access_token;;
+            console.log(response.access_token);
+            return response.access_token;
         }
 
         /**
@@ -28,20 +28,27 @@ export module PayPal {
          * @see https://developer.paypal.com/docs/regional/br/create-payment-request/
          */
         public async createPaymentRequest(paymentRequest: dto.PayPalDTO): Promise<dto.PayPalDTO> {
-            let endpointUrl = `/payments/payment`;
             let accessToken = await this.getAccessToken();
+            let jsonRequest = JSON.stringify(paymentRequest);
 
-            let request = await fetch(`${Configuration.PAYPAL_URL}${endpointUrl}`,
+            let request = await fetch(`${Configuration.PAYPAL_URL}/v1/payments/payment`,
                 {
                     method: 'POST',
-                    body: JSON.stringify(paymentRequest),
+                    body: jsonRequest,
                     headers: {
-                        Authorization: `Bearer ${accessToken}`
+                        Authorization: `Bearer ${accessToken}`,
+                        "Content-Type": 'application/json'
                     }
                 }
             );
 
-            let response: dto.PayPalDTO = await request.json();
+            let response: dto.PayPalDTO = {};
+            try {
+                response = await request.json();
+            }
+            catch (error) {
+                console.warn(error);
+            }
 
             return response;
         }
@@ -73,94 +80,154 @@ export module PayPal {
          * @see https://developer.paypal.com/docs/regional/br/test-and-execute/
          */
         public async executeOrder(paymentRequest: dto.PayPalDTO): Promise<dto.PayPalDTO> {
-            return {} as dto.PayPalDTO;
+            let accessToken = await this.getAccessToken();
+
+            let request = await fetch(`${Configuration.PAYPAL_URL}/payments/payment/${paymentRequest.payer!.payer_info!.payer_id!}/execute`,
+                {
+                    method: 'POST',
+                    body: JSON.stringify(paymentRequest),
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`
+                    }
+                }
+            );
+
+            let response: dto.PayPalDTO = await request.json();
+
+            return response;
         }
     }
 
     export module dto {
-
         export interface PayPalDTO {
-            id: string;
-            create_time: string;
-            update_time: string;
-            state: string;
-            intent: string;
-            payer: Payer;
-            transactions: Transaction[];
-            redirect_urls: RedirectUrls;
-            links: Link[];
+            id?: string;
+            intent?: string;
+            state?: string;
+            cart?: string;
+            payer?: Payer;
+            transactions?: Transaction[];
+            failed_transactions?: any[];
+            create_time?: string;
+            update_time?: string;
+            links?: Link[];
+            redirect_urls?: RedirectUrls;
         }
 
         export interface Payer {
-            payment_method: string;
-            payer_info: PayerInfo;
+            payment_method?: string;
+            status?: string;
+            payer_info?: PayerInfo;
         }
 
         export interface PayerInfo {
-            shipping_address: ShippingAddress;
-        }
-
-        export interface Transaction {
-            amount: Amount;
-            description: string;
-            payment_options: PaymentOptions;
-            item_list: ItemList;
-        }
-
-        export interface Amount {
-            currency: string;
-            total: string;
-            details: Details;
-        }
-
-        export interface Details {
-            shipping: string;
-            subtotal: string;
-            shipping_discount: string;
-            insurance: string;
-            handling_fee: string;
-            tax: string;
-        }
-
-        export interface PaymentOptions {
-            allowed_payment_method: string;
-        }
-
-        export interface ItemList {
-            shipping_address: ShippingAddress;
-            items: Item[];
+            email?: string;
+            first_name?: string;
+            last_name?: string;
+            payer_id?: string;
+            shipping_address?: ShippingAddress;
+            tax_id_type?: string;
+            tax_id?: string;
+            country_code?: string;
         }
 
         export interface ShippingAddress {
-            recipient_name: string;
-            line1: string;
-            line2: string;
-            city: string;
-            country_code: string;
-            postal_code: string;
-            state: string;
-            phone: string;
+            recipient_name?: string;
+            line1?: string;
+            line2?: string;
+            city?: string;
+            state?: string;
+            postal_code?: string;
+            country_code?: string;
+            normalization_status?: string;
+            phone?: string;
+        }
+
+        export interface Transaction {
+            amount?: Amount;
+            payee?: Payee;
+            description?: string;
+            soft_descriptor?: string;
+            item_list?: ItemList;
+            related_resources?: RelatedResource[];
+            payment_options?: PaymentOptions;
+        }
+
+        export interface PaymentOptions {
+            allowed_payment_method?: string;
+        }
+
+
+        export interface Amount {
+            total?: string;
+            currency?: string;
+            details?: Details;
+        }
+
+        export interface Details {
+            subtotal?: string;
+            tax?: string;
+            shipping?: string;
+            insurance?: string;
+            handling_fee?: string;
+            shipping_discount?: string;
+            discount?: string;
+        }
+
+        export interface Payee {
+            merchant_id?: string;
+            email?: string;
+        }
+
+        export interface ItemList {
+            items?: Item[];
+            shipping_address?: ShippingAddress;
+            shipping_phone_number?: string;
         }
 
         export interface Item {
-            name: string;
-            description: string;
-            quantity: string;
-            price: string;
-            tax: string;
-            sku: string;
-            currency: string;
+            name?: string;
+            sku?: string;
+            description?: string;
+            price?: string;
+            currency?: string;
+            tax?: string;
+            quantity?: number;
+            image_url?: string;
         }
 
-        export interface RedirectUrls {
-            return_url: string;
-            cancel_url: string;
+        export interface RelatedResource {
+            sale?: Sale;
+        }
+
+        export interface Sale {
+            id?: string;
+            state?: string;
+            amount?: Amount;
+            payment_mode?: string;
+            protection_eligibility?: string;
+            protection_eligibility_type?: string;
+            transaction_fee?: TransactionFee;
+            parent_payment?: string;
+            create_time?: string;
+            update_time?: string;
+            links?: Link[];
+            soft_descriptor?: string;
+        }
+
+        export interface TransactionFee {
+            value?: string;
+            currency?: string;
         }
 
         export interface Link {
-            href: string
-            rel: string
-            method: string
+            href?: string;
+            rel?: string;
+            method?: string;
+        }
+
+        export interface RedirectUrls {
+            return_url?: string;
+            cancel_url?: string;
         }
     }
 }
