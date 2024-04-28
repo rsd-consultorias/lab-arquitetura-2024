bd2904b4da4326162ae366887be834bca353980c
Rafael Dias <rafael.dias@rsdconsultoria.com.br>
2024-04-28 13:02:21 -0300

    bug fix

--- nodejs/src/index.ts
+++ nodejs/src/index.ts
@@ -1,12 +1,18 @@
 import { CheckoutController } from "./controllers/checkout.controller";
 import { HttpServer } from "./infra/http-server";
+import { AccountQueue } from "./infra/message-broker/account.queue";
+import { SubscriptionQueue } from "./infra/message-broker/subscription.queue";
 import { CheckoutRepository } from "./infra/repositories/checkout-repository";
 
 export function initServer(port: number) {
     const httpServer = new HttpServer();
     const checkoutRepository = new CheckoutRepository();
     
-    new CheckoutController(checkoutRepository, httpServer);
+    new CheckoutController(
+        checkoutRepository, 
+        new AccountQueue(),
+        new SubscriptionQueue(),
+        httpServer);
 
     httpServer.listen(port);
 }
